9e700f584030c8c3b48e319ed9329c236c34b3a5
Amansingh1811 <amansingh18112000@gmai.com>
2022-02-18 11:25:45 +0530

    fetch API through async await

---
+++ projects/Jokes/jokes.js
@@ -0,0 +1,26 @@
+const jokes = document.querySelector('#jokes')
+const jokesBtn = document.querySelector('#jokesBtn')
+ 
+const generatejokes = async () =>{
+    try{
+        const setHeader ={
+            headers: {
+                Accept : "application/json"
+            }
+        }
+        // fetch('https://icanhazdadjoke.com/', setHeader)
+        // .then((res) => res.json())
+        // .then((data) =>{
+        //     jokes.innerHTML = data.joke;
+        // }).catch((error) =>
+        // console.log(error))
+        const res = await fetch('https://icanhazdadjoke.com/', setHeader)
+        const data = await res.json();
+        jokes.innerHTML = data.joke;
+        }catch(err){
+            console.log(` its shows an error ${err}`);
+            }
+        }
+    jokesBtn.addEventListener('click', generatejokes)
+    generatejokes();
+
